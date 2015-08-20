c765198be71eb5cf84cd56c3dc5b4a0bcefaef86
alkhammsh <heru2680@gmail.com>
2015-08-20 00:18:04 +0000

    first offical commit to github

---
+++ mathwithpython.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# learning to do math with python 
+# with the folowing equations
+# google the term : geomtry equations
+h=10
+b=2
+A=.5*b*h
+print A
+print "the area of trinagle with h=10 ans b=2"
+
