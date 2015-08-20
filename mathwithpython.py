099320d1e974007a178a93fdb1cf086e81e71106
alkhammsh <heru2680@gmail.com>
2015-08-20 00:29:29 +0000

    just some thought i added to the program

--- mathwithpython.py
+++ mathwithpython.py
@@ -2,9 +2,19 @@
 # learning to do math with python 
 # with the folowing equations
 # google the term : geomtry equations
+
+
+
 h=10
 b=2
 A=.5*b*h
 print A
 print "the area of trinagle with h=10 ans b=2"
 
+# im really starting to enjoy programing 
+# i love women 
+# i love big butts
+# it when really well today i learned all of what i need to get me started.
+# but after work we have to go out for drinks an get bitches.
+# an if we get lucky we have to do our special high five dance.
+# an must leave hand prints on there asses.
