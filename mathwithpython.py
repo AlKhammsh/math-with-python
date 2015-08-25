df431ab503ed47573588bc5064e855a6dbec0aa5
alkhammsh <heru2680@gmail.com>
2015-08-25 02:53:20 +0000

    i fix my notes section

--- mathwithpython.py
+++ mathwithpython.py
@@ -11,10 +11,3 @@ A=.5*b*h
 print A
 print "the area of trinagle with h=10 ans b=2"
 
-# im really starting to enjoy programing 
-# i love women 
-# i love big butts
-# it when really well today i learned all of what i need to get me started.
-# but after work we have to go out for drinks an get bitches.
-# an if we get lucky we have to do our special high five dance.
-# an must leave hand prints on there asses.
